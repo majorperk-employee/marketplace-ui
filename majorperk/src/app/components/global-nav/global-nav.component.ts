411bd2e8847bdef49c08b80010408e255aef1e00
Sergei Hanka <sergeiahanka@gmail.com>
2019-04-14 21:11:54 -0500

    added 3 cols

--- majorperk/src/app/components/global-nav/global-nav.component.ts
+++ majorperk/src/app/components/global-nav/global-nav.component.ts
@@ -4,22 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-global-nav',
   templateUrl: './global-nav.component.html',
-  styles: [
-    `
-      .logo {
-        width: 120px;
-        height: 31px;
-        background: rgba(255, 255, 255, 0.2);
-        margin: 16px 24px 16px 0;
-        float: left;
-      }
-    `
-  ]
+  styleUrls: ['./global-nav.component.scss']
 })
 export class GlobalNavComponent {
-
   constructor(private router: Router) { }
-  
   routeTo(destination) {
     this.router.navigate([destination]);
   }
