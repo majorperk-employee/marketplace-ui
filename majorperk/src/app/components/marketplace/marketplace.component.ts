136d33324bf27f8bf93ebe6ab3a1a66f9c84e6ff
Luke Snell <ljsnell7@gmail.com>
2019-04-17 00:40:48 -0500

    Suspiciously familiar example of reading data from a .ts file. Going to convert to a list of cards.
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- majorperk/src/app/components/marketplace/marketplace.component.ts
+++ majorperk/src/app/components/marketplace/marketplace.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketplaceComponent implements OnInit {
 
+  data: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.data = marketLinks;
   }
-
 }
+
+let marketLinks = [
+  {
+    href: "/marketplace/travel",
+    title: `Travel`,
+    avatar: '',
+    description: 'Redeem rewards points towards a vacation',
+  },
+  {
+    href: "/marketplace/gift card",
+    title: `Gift Cards`,
+    avatar: '',
+    description: 'Turn those rewards points into credit at your favorite store',
+  },
+  {
+    href: "/marketplace/fitness",
+    title: `Fitness`,
+    avatar: '',
+    description: 'Reward your body with rewards points',
+  },
+  {
+    href: "/marketplace/electronics",
+    title: `Electronics`,
+    avatar: '',
+    description: 'Reward yourself with new gadgets',
+  },
+  {
+    href: "/marketplace/transportation",
+    title: `Transportation`,
+    avatar: '',
+    description: 'Give your rewards some miles',
+  },
+  {
+    href: "/userprofile/redeem",
+    title: `Cash Out`,
+    avatar: '',
+    description: 'Redeem your points for a VISA gift card',
+  }
+]
+
+let resources = [
+  {
+    display: "Affordable Housing Partners",
+    url: "https://www.toronto.ca/community-people/community-partners/affordable-housing-partners/",
+    description: "Toronto affordable housing partners, locate homes and new developments in the area."
+  },
+  {
+    display: "Child Care & Before-After School Program Locator",
+    url: "https://www.toronto.ca/community-people/children-parenting/children-programs-activities/licensed-child-care/child-care-locator/",
+    description: "Search for a licensed child care or a before-after school program in Toronto."
+  }
+]
