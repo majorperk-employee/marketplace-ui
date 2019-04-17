598cdf814b7d638bf28c15bf9f6ce2c92528c80d
Luke Snell <ljsnell7@gmail.com>
2019-04-17 01:32:56 -0500

    Updated with images. Almost looks like it's supposed to.
    Signed-off-by: Luke Snell <ljsnell7@gmail.com>

--- majorperk/src/app/components/marketplace/marketplace.component.ts
+++ majorperk/src/app/components/marketplace/marketplace.component.ts
@@ -12,39 +12,39 @@ export class MarketplaceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.data = marketLinks;
+    this.data = marketItems;
   }
 }
 
-let marketLinks = [
+let marketItems = [
   {
     title: `Weekend Getaway - New York`,
-    image: '',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   },
   {
     title: `Weekend Getaway - Minneapolis`,
-    image: '',
+    image: 'http://exclaim.ca/images/dabbing.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   },
   {
     title: `Weekend Getaway - Boston`,
-    image: '',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   },
   {
     title: `$5 Amazon`,
-    image: '',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   },
   {
     title: `$10 Amazon`,
-    image: '',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   },
   {
     title: `$20 Amazon`,
-    image: '',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/6101Je6svzL._SX425_.jpg',
     description: 'Lorem ipsum dolor sit amet, ne vis quem graeco consetetur, id eam doming admodum epicurei. Mei tritani mandamus scriptorem cu, labore insolens platonem id eam.',
   }
 ]
