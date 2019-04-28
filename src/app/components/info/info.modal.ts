26dc251a174d2f8921ec781142bb96855599abe1
Sergei Hanka <sergeiahanka@gmail.com>
2019-04-27 22:46:56 -0500

    add to cart functionality

--- src/app/components/info/info.modal.ts
+++ src/app/components/info/info.modal.ts
@@ -2,6 +2,8 @@
 
 import { Component, Input, TemplateRef } from '@angular/core';
 import { NzModalRef, NzModalService } from 'ng-zorro-antd';
+import { RewardModalComponent } from '../reward-modal/reward-modal.component';
+import { Brand } from '@app/models/market';
 
 @Component({
   selector: 'app-info-modal',
@@ -15,7 +17,14 @@ export class InfoModalComponent {
   htmlModalVisible = false;
   disabled = false;
 
-  constructor(private modalService: NzModalService) {}
+  rewardModal: NzModalRef;
+  rewardModalLoading = false;
+  rewardModalVisible = false;
+
+  isVisible = false;
+  itemId: number;
+
+  constructor(private modalService: NzModalService) { }
 
   createTemplatedModal(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
     this.tplModal = this.modalService.create({
@@ -26,11 +35,41 @@ export class InfoModalComponent {
       nzClosable: false,
       nzOnOk: () => console.log('Click ok')
     });
+  }
 
-    // this.tplModal.afterOpen.subscribe(() => console.log('[afterOpen] emitted!'));
+  createTplModal(tplTitle: TemplateRef<{}>, tplContent: TemplateRef<{}>, tplFooter: TemplateRef<{}>): void {
+    this.tplModal = this.modalService.create({
+      nzTitle: tplTitle,
+      nzContent: tplContent,
+      nzFooter: tplFooter,
+      nzMaskClosable: false,
+      nzClosable: false,
+      nzOnOk: () => console.log('Click ok')
+    });
+  }
 
-    // Return a result when closed
-    // this.tplModal.afterClose.subscribe(result => console.log('[afterClose] The result is:', result));
+  destroyTplModal(): void {
+    this.tplModalButtonLoading = true;
+    setTimeout(() => {
+      this.tplModalButtonLoading = false;
+      this.tplModal.destroy();
+    }, 1000);
+  }
+
+  createRewardModal(brand: Brand): void {
+    let itemId = brand.id;
+    const modal = this.modalService.create({
+      nzTitle: `${brand.brandName}`,
+      nzContent: RewardModalComponent,
+      nzComponentParams: {
+        id: itemId,
+      },
+      nzFooter: [{
+        label: 'Thanks!',
+        shape: 'primary',
+        onClick: () => modal.destroy()
+      }]
+    });
   }
 
   destroyTemplatedModal(): void {
---
+++ src/app/components/reward-modal/reward-modal.component.ts
@@ -0,0 +1,103 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { NzModalRef } from 'ng-zorro-antd';
+import { Brand, RewardItem, Cart, Meta } from '@app/models/market';
+import { ShoppingService } from '@app/service/shopping.service';
+import { first } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { AccountService } from '@app/service/account.service';
+import { Account } from '@app/models/account';
+
+@Component({
+  selector: 'app-reward-modal',
+  templateUrl: './reward-modal.component.html',
+  styleUrls: ['./reward-modal.component.scss']
+})
+export class RewardModalComponent implements OnInit, OnDestroy {
+  @Input() id: number;
+
+  account: Account;
+  cart: Cart;
+  brand: Brand;
+  currentItem: RewardItem;
+
+  loading: boolean = true;
+  error: boolean = false;
+  itemSubscription: Subscription;
+
+  constructor(private modal: NzModalRef, private shoppingService: ShoppingService, private accountService: AccountService) { }
+
+  ngOnInit() {
+
+    this.account = this.accountService.currentAccount;
+
+    this.itemSubscription = this.shoppingService.getReward(this.id).pipe(first()).subscribe(
+      (resp: Brand) => {
+        console.log(resp);
+        if (resp.items) { resp.items.forEach(item => item.meta = new Meta(this.inCart(item.id))) };
+        this.brand = resp; 
+        this.loading = false;
+      },
+      error => { this.loading = false; this.error = true; }
+    );
+
+  }
+
+  addToCart(item: RewardItem) {
+
+    this.shoppingService.addToCart(item.id, this.account.id);
+
+    // REFRESH ACCOUNT
+    this.account = this.accountService.currentAccount;
+
+    item.meta.checked = true;
+
+  }
+
+  removeFromCart(item: RewardItem) {
+
+    let removeBody = [item.id];
+
+    this.shoppingService.removeFromCart(removeBody, this.account.id);
+
+    // REFRESH ACCOUNT
+    this.account = this.accountService.currentAccount;
+
+    console.log(this.account);
+
+    item.meta.checked = false;
+  }
+
+  inCart(itemId: number): boolean {
+    let incart = false;
+    this.account.cart.items.forEach((item: RewardItem) => {
+      if (item.id == itemId) {
+        incart = true;
+      }
+    });
+    return incart;
+  }
+
+  setItem(item: RewardItem) {
+    this.currentItem = item;
+  }
+
+  ngOnDestroy() {
+    this.destroyModal()
+  }
+
+  destroyModal(): void {
+    this.currentItem = null;
+    this.itemSubscription.unsubscribe();
+    this.modal.destroy();
+  }
+
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+}
